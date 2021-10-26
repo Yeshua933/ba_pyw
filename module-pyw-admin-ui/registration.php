5efda93fb828444f8394bc061c79393ec1b7fbcf
Marcel Martinez <martinezlopezmarcel@gmail.com>
2021-10-26 16:32:43 -0500

    Reduce modules to one

--- module-pyw-admin-ui/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author    Blue Acorn iCi <code@blueacornici.com>
- * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
- */
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'PayYourWay_PywAdminUi', __DIR__);
