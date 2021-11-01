13061653b6227558c69234a3939487e85c6b369f
Yeshua Torreblanca <yeshuatorreblanca@admins-MBP.attlocal.net>
2021-11-01 10:58:24 -0400

    added Configuration fields for payment method. Depend attribute for sandbox fields.

---
+++ Block/Adminhtml/System/Config/KeyGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @author    Blue Acorn iCi <code@blueacornici.com>
+ * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
+ */
+
+namespace PayYourWay\Pyw\Block\Adminhtml\System\Config;
+
+use Magento\Backend\Block\Template\Context;
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+/**
+ * Custom renderer for PYW Onboarding popup
+ */
+class KeyGenerator extends Field
+{
+    /**
+     * Path to block template
+     */
+    protected $_template = 'PayYourWay_Pyw::system/config/KeyGenerator.phtml';
+
+    public function __construct(Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+    }
+
+    public function render(AbstractElement $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        return $this->_toHtml();
+    }
+    public function getCustomUrl()
+    {
+        return $this->getUrl('router/controller/action');
+    }
+    public function getButtonHtml()
+    {
+        $button = $this->getLayout()->createBlock('Magento\Backend\Block\Widget\Button')->setData(['id' => 'generate_key', 'label' => __('Generate Key'),]);
+        return $button->toHtml();
+    }
+}
