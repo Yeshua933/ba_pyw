05e770d116609ae9f88d75953640301fd7a8a133
Marcel Martinez <martinezlopezmarcel@gmail.com>
2021-11-17 18:40:13 -0600

    PAC: Replace token urls

--- Model/GenerateAccessToken.php
+++ Model/GenerateAccessToken.php
@@ -36,8 +36,8 @@ class GenerateAccessToken implements GenerateAccessTokenInterface
         $client_id = $this->config->getClientId();
         $privateKey = $this->config->getPrivateKey();
         $aud = $this->config->getEnvironment() === Environment::ENVIRONMENT_SANDBOX ?
-            "https://oauth.uat.telluride.transformco.com/oauthAS/service/oAuth/token.json" :
-            "https://oauth.telluride.transformco.com/oauthAS/service/oAuth/token.json";
+            self::OAUTH_UAT :
+            self::OAUTH_PRD;
 
         $claim = $this->getEncoded(
             json_encode([
