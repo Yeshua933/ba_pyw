4a5f4104e51245af58c3efd51946e571f105b920
Yeshua Torreblanca <yeshuatorreblanca@admins-MBP.attlocal.net>
2021-11-29 14:20:53 -0500

    Add data patch for payment title.

---
+++ Setup/Patch/Data/AddTitle.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author    Blue Acorn iCi <code@blueacornici.com>
+ * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
+ */
+
+namespace PayYourWay\Pyw\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+class AddTitle implements DataPatchInterface, PatchVersionInterface
+{
+
+    private ModuleDataSetupInterface $moduleDataSetup;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public static function getVersion(): string
+    {
+        return '1.0.1';
+    }
+
+    public function apply()
+    {
+        $this->moduleDataSetup->startSetup();
+        $this->moduleDataSetup->getConnection()->insert("core_config_data",
+            ["scope" => "default",
+                "scope_id" => 0,
+                "path" => "payment/payyourway/title",
+                "value" => "Pay with Shop Your Way"
+            ]
+        );
+        $this->moduleDataSetup->endSetup();
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
