8bafa502ff231bbb1d0f931e4db31a997fbfb163
Yeshua Torreblanca <yeshuatorreblanca@admins-MBP.attlocal.net>
2021-12-22 07:58:00 -0500

    PAC-207 terms and conditions

---
+++ Block/Adminhtml/System/Config/Checkbox.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @author    Blue Acorn iCi <code@blueacornici.com>
+ * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
+ */
+namespace PayYourWay\Pyw\Block\Adminhtml\System\Config;
+
+use Magento\Config\Block\System\Config\Form\Field;
+
+class Checkbox extends Field
+{
+    const CONFIG_PATH = 'payment/payyourway/checkbox';
+
+    protected $_template = 'PayYourWay_Pyw::system/config/checkbox.phtml';
+
+    protected $_values = null;
+    /**
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param \Magento\Config\Model\Config\Structure $configStructure
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+    /**
+     * Retrieve element HTML markup.
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     *
+     * @return string
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $this->setNamePrefix($element->getName())
+            ->setHtmlId($element->getHtmlId());
+
+        return $this->_toHtml();
+    }
+
+    public function getValues()
+    {
+        $values = [];
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        foreach ($objectManager
+                     ->create('PayYourWay\Pyw\Model\Adminhtml\Config\Source\Checkbox')
+                     ->toOptionArray() as $value) {
+
+            $values[$value['value']] = $value['label'];
+        }
+
+        return $values;
+    }
+    /**
+     *
+     * @param  $name
+     * @return boolean
+     */
+    public function getIsChecked($name)
+    {
+        return in_array($name, $this->getCheckedValues());
+    }
+    /**
+     *
+     * get the checked value from config
+     */
+    public function getCheckedValues()
+    {
+        if (is_null($this->_values)) {
+            $data = $this->getConfigData();
+            if (isset($data[self::CONFIG_PATH])) {
+                $data = $data[self::CONFIG_PATH];
+            } else {
+                $data = '';
+            }
+            $this->_values = explode(',', $data);
+        }
+
+        return $this->_values;
+    }
+}
