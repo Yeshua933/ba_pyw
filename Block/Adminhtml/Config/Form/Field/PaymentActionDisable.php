cf438236c058922632622e93f68a3efc9905fbab
Marcel Martinez <marcelmartinez@Marcels-MacBook-Pro.local>
2021-12-27 10:38:34 -0600

    PAC-211: Added payment action button disabled

---
+++ Block/Adminhtml/Config/Form/Field/PaymentActionDisable.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author    Blue Acorn iCi <code@blueacornici.com>
+ * @copyright 2021 Blue Acorn iCi. All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace PayYourWay\Pyw\Block\Adminhtml\Config\Form\Field;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class PaymentActionDisable extends \Magento\Config\Block\System\Config\Form\Field
+{
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $element->setDisabled('disabled');
+        return $element->getElementHtml();
+    }
+}
